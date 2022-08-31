cac4cf5f8ab2d3f8e35a89434d7a19ad615b71a6
Feyerabend <set@setlonnert.com>
2022-08-31 20:19:02 +0200

    Update asm.py

--- enkel0/asm.py
+++ enkel0/asm.py
@@ -1,30 +1,6 @@
 import sys
+import getopt
 import re
-
-if len(sys.argv) != 3:
-    print("Usage: asm.py <input> <output>")
-    exit()
-
-with open(sys.argv[1]) as f:
-    content = f.readlines()
-
-def no_comments(content):
-    ncontent = []
-    for line in content:
-        temp = re.sub('#.*', '', line)
-        ncontent.append(temp)
-    return ncontent
-
-# no comments and other stuff
-def prepare(content):
-    content = no_comments(content)
-    # no new lines
-    content = [' '.join(line.split()) for line in content]
-    # no white space
-    content = [line for line in content if line.strip()]
-    # group and "tokenize"
-    content = [line.split() for line in content if line]
-    return content
 
 # must be in sync with vm.h
 ops = [
@@ -99,11 +75,29 @@ ary = [
     0,      # UMIN
     0]      # XOR
 
+def no_comments(content):
+    ncontent = []
+    for line in content:
+        temp = re.sub('#.*', '', line)
+        ncontent.append(temp)
+    return ncontent
+
+# no comments and other stuff
+def prepare(content):
+    content = no_comments(content)
+    # no new lines
+    content = [' '.join(line.split()) for line in content]
+    # no white space
+    content = [line for line in content if line.strip()]
+    # group and "tokenize"
+    content = [line.split() for line in content if line]
+    return content
+
 # turn to decimal
 def to_decimal(number):
     return int(number)
 
-# replace the corresponding opcodes with numbers, and arguments
+# replace the corresponing opcodes with numbers, and arguments
 def parse(line):
     if (line[0] in ops):
         nline = []
@@ -117,43 +111,82 @@ def parse(line):
     else:
         return line
 
-# main:
-# prep
-content = prepare(content)
+# assemble binary
+def assemble(inputfile, outputfile, verbose):
 
-# parse
-ncontent = []
-for item in range(len(content)):
-    line = content[item]
-    line = parse(line)
-    ncontent.append(line)
-content = ncontent
+    # read in file
+    with open(inputfile) as f:
+        content = f.readlines()
 
-# hunt for labels
-ncontent = []
-labels = {} # a dictionary of labels
-offset = 0 # begin at address zero
-for item in [val for sublist in content for val in sublist]:
-    islabel = (str(item)[-1] == ':') # ex. START:
-    if islabel:
-        matchlabel = (':' + item[:-1])
-        labels[matchlabel] = offset # ex. :START
-    else:
-        offset = offset + 1
-        ncontent.append(item)
+    # prep
+    content = prepare(content)
 
-# replace labels with their offset
-ncontent = [labels[token] if token in labels else token for token in ncontent]
+    # parse
+    ncontent = []
+    for item in range(len(content)):
+        line = content[item]
+        line = parse(line)
+        ncontent.append(line)
+    content = ncontent
 
-# convert some remains to decimal
-final = [to_decimal(i) for i in ncontent]
+    # hunt for labels
+    ncontent = []
+    labels = {} # a dictionary of labels
+    offset = 0 # begin at address zero
+    for item in [val for sublist in content for val in sublist]:
+        islabel = (str(item)[-1] == ':') # ex. START:
+        if islabel:
+            matchlabel = (':' + item[:-1])
+            labels[matchlabel] = offset # ex. :START
+        else:
+            offset = offset + 1
+            ncontent.append(item)
 
-print("assembling ..")
-print(final)
-print("START: ", labels[':START'])
+    # replace labels with their offset
+    ncontent = [labels[token] if token in labels else token for token in ncontent]
 
-final.insert(0, labels[':START'])
-final_str = [str(int) for int in final]
-with open(sys.argv[2], "w") as f:
-    f.write(','.join(final_str))
-print("assembling done.")
+    # convert some remains to decimal
+    final = [to_decimal(i) for i in ncontent]
+
+    if verbose == 1:
+        print(final)
+        print("START: ", labels[':START'])
+
+    final.insert(0, labels[':START'])
+    final_str = [str(int) for int in final]
+    with open(outputfile, "w") as f:
+        f.write(','.join(final_str))
+
+
+# call with parsing of args to assembler
+def main(argv):
+    inputfile = ''
+    outputfile = ''
+    verbose = 0
+
+    try:
+        opts, args = getopt.getopt(argv,"vhi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print('asm.py -i <inputfile> -o <outputfile>')
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-v':
+            verbose = 1
+        if opt == '-h':
+            print('usage: asm.py -i <inputfile> -o <outputfile>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+
+    if verbose == 1:
+        print("assembling ..")
+    assemble(inputfile, outputfile, verbose)
+    if verbose == 1:
+        print("done.")
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
