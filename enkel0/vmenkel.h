cff931bce16db0b1c4472e8c45994274bff258de
Feyerabend <set@setlonnert.com>
2022-09-10 00:55:02 +0200

    Update vmenkel.h

--- enkel0/vmenkel.h
+++ enkel0/vmenkel.h
@@ -24,35 +24,35 @@ enum {
 	CALL,	// 2
 	DIV,	// 4
 	EMIT,	// 5
-	EQ,		// 6
-	GT,		// 7
-	GQ,		// 8
+	EQ,	// 6
+	GT,	// 7
+	GQ,	// 8
 	HALT,	// 9
-	JP,		// 10
+	JP,	// 10
 	JPNZ,	// 11
 	JPZ,	// 12
-	LD,		// 13
+	LD,	// 13
 	LDARG,	// 14
 	LOAD,	// 15
-	LT,		// 16
-	LQ,		// 17
+	LT,	// 16
+	LQ,	// 17
 	MOD,	// 18
 	MUL,	// 19
 	NEQ,	// 20
 	NOP,	// 21
-	OR,		// 22
+	OR,	// 22
 	PRINT,	// 23
 	PRNT,	// 24
 	RET,	// 25
 	RLOAD,	// 26
 	RSTORE,	// 27
 	SET,	// 28
-	ST,		// 29
+	ST,	// 29
 	STARG,	// 30
 	STORE,	// 31
 	SUB,	// 32
 	UMIN,	// 33
-	XOR		// 34
+	XOR	// 34
 };
 
 VM* newVM(int* code, int pc, int vars, int args, int arrs);
