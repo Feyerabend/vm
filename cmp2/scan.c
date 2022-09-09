d012a281af6413f8a3ac98ce8c0eb4a1da201867
Feyerabend <set@setlonnert.com>
2022-09-10 01:20:55 +0200

    Update scan.c

--- cmp2/scan.c
+++ cmp2/scan.c
@@ -6,39 +6,6 @@
 
 #include "scan.h"
 
-/*
-    NOP,
-    IDENT,
-    NUMBER,
-    LPAREN,     // (
-    RPAREN,     // )
-    TIMES,      // *
-    SLASH,      // /
-    PLUS,       // +
-    MINUS,      // -
-    EQL,        // =
-    NEQ,        // #
-    LSS,        // <
-    LEQ,        // <=
-    GTR,        // >
-    GEQ,        // >=
-    CALLSYM,    // call
-    BEGINSYM,   // begin
-    SEMICOLON,  // ;
-    ENDSYM,     // end
-    IFSYM,      // if
-    WHILESYM,   // while
-    BECOMES,    // :=
-    THENSYM,    // then
-    DOSYM,      // do
-    CONSTSYM,   // const
-    COMMA,      // ,
-    VARSYM,     // var
-    PROCSYM,    // proc
-    PERIOD,     // .
-    ODDSYM,     // odd
-    ENDOFFILE
-*/
 
 char buf[MAXSYMB];
 
