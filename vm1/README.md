d1f5ddb7cb33d06b6b1ffcdc0bb788d891c51259
Feyerabend <set@setlonnert.com>
2022-09-24 00:53:06 +0200

    Another small restricted vm

--- vm1/README.md
+++ vm1/README.md
@@ -77,13 +77,13 @@ You should be able to see what 33 + 44 is ... (hint 77)
 
 One of the many praised features of virtual machines is enabling *portability of code*.
 You can try another version of the virtual machine in "alternate-vm.html", which you
-can run in your browser. (Copy to the desktop and double-click, check through the developer
-option in your browser the output from "console.log".)
+can run in your browser. Copy to the desktop and double-click, check through the developer
+option in your browser the output from "console.log". (Another stripped and rudimentary
+implementation is in "alternate-vm.py", which you can run if Python >3.10 is installed.)
 You will notice the program is the same in *principle* (as the opcodes can also be) so we only
 implement a new virtual machine each time we would like to run the program on another
 system, operating system, hardware, etc. *The code for the program then can remain the same.*
-Sun Microsystems Inc., the company originally
-behind the programming language Java, once had a slogan: "Write Once, Run Anywhere"[^1],
-that kind of illustrates that idea.
+Sun Microsystems Inc., the company originally behind the programming language Java,
+once had a slogan: "Write Once, Run Anywhere"[^1], that kind of illustrates that idea.
 
 [^1]: https://en.wikipedia.org/wiki/Write_once,_run_anywhere
---
+++ vm1/alternate-vm.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
+
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        self.stack.append(value)
+    
+    def pop(self):
+        return self.stack.pop()
+
+
+class Emulator:
+
+    def __init__(self):
+        self.stack = Stack()
+
+    def mul(self):
+        b = self.stack.pop()
+        a = self.stack.pop()
+        c = int(a) * int(b)
+        self.stack.push(c)
+
+    def add(self):
+        b = self.stack.pop()
+        a = self.stack.pop()
+        c = int(a) + int(b)
+        self.stack.push(c)
+
+    def sub(self):
+        b = self.stack.pop()
+        a = self.stack.pop()
+        c = int(a) - int(b)
+        self.stack.push(c)
+
+    def set(self, number):
+        self.stack.push(number)
+
+    def printn(self):
+        a = self.stack.pop()
+        print(a)
+
+
+    def parse(self, command):
+    
+        match command.split():
+
+            case ['MUL']:
+                self.mul()
+
+            case ['ADD']:
+                self.add()
+
+            case ['SUB']:
+                self.sub()
+
+            case ['SET', number]:
+                self.set(number)
+
+            case ['PRINT']:
+                self.printn()
+
+            case ['HALT']:
+                sys.exit(0)
+
+            case _:  # catch all
+                print(f"{command!r}?")
+
+
+class Runner:
+
+    def __init__(self):
+        self.emu = Emulator()
+
+    def read(self, file):
+        with open(file) as f:
+            contents = f.readlines()
+            print(contents)
+            for line in contents:
+                command = line.strip()
+                self.emu.parse(command)
+        f.close()
+
+
+run = Runner()
+run.read('sample.txt')
