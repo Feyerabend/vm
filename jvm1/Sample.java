8a62b11e547fe1e20ac83d499cb4a6b1a81b40a2
Feyerabend <set@setlonnert.com>
2023-08-27 15:47:45 +0200

    Add

---
+++ jvm1/Sample.java
@@ -0,0 +1,9 @@
+
+public class Sample {
+    public static void main(String[] args) {
+        System.out.println(add(23, 45));
+    }
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
