f0cfb763556d6513f919e44fc2302ad1e9f5e991
Feyerabend <set@setlonnert.com>
2023-08-29 22:52:47 +0200

    Update classread-sample.py

--- jvm1/classread-sample.py
+++ jvm1/classread-sample.py
@@ -4,9 +4,7 @@
 import struct
 
 with open("Sample.class", "rb") as f:
-    magic = f.read(4)
-    if magic != b"\xCA\xFE\xBA\xBE":
-        raise ValueError("Invalid class file")
-    minor_version = struct.unpack("<H", f.read(2))[0]
-    major_version = struct.unpack("<H", f.read(2))[0]
-    print(f"version: {major_version}.{minor_version}")
+    # CAFEBABE and version
+    magic = struct.unpack('!I', cf.read(4))
+    minor, major = struct.unpack('!HH', cf.read(4))
+    print(f"version: {major}.{minor}")
