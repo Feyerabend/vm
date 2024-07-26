5eb1941a6996523197727aaa3ffde9e54e2150e1
Feyerabend <setlonnert@gmail.com>
2024-07-26 18:16:58 +0200

    Update sample.d

--- debug/sample.d
+++ debug/sample.d
@@ -3,25 +3,25 @@ L0:
 	ST 0		# 028 000
 	LD 0		# 012 000
 	SET 1		# 027 001
-	EQ		# 005
+	EQ		    # 005
 	JPZ :L16	# 011 016
 	SET 1		# 027 001
 	STORE 0		# 030 000
-	RET		# 024
+	RET		    # 024
 L16:
 	LD 0		# 012 000
 	SET 1		# 027 001
-	SUB		# 031
+	SUB		    # 031
 	ST 3		# 028 003
 	LD 3		# 012 003
 	STARG 0		# 029 000
 	CALL :L0	# 002 000
 	LD 0		# 012 000
 	LOAD 0		# 014 000
-	MUL		# 018
+	MUL		    # 018
 	STORE 0		# 030 000
-	RET		# 024
-	RET		# 024
+	RET		    # 024
+	RET		    # 024
 START:
 	SET 10		# 027 010
 	STARG 0		# 029 000
